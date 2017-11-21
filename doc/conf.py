277387b4e6ebb8bb92e4945701530daed46cbe49
gausspy <gausspy.software@gmail.com>
2017-11-21 16:16:04 -0500

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,7 @@ extensions = [
     'sphinx.ext.imgmath',
     #'sphinx.ext.mathjax',
     'sphinx.ext.autodoc',
-    'numfig'
+    #'numfig'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
