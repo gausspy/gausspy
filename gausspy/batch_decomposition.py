daa6b612067a3b9ef0a99fd47a2e73051ac97887
cmurray-astro <clairemurray56@gmail.com>
2019-07-31 15:06:34 -0400

    Fixed formatting of all .py files using black

--- gausspy/batch_decomposition.py
+++ gausspy/batch_decomposition.py
@@ -8,35 +8,40 @@ from .gp import GaussianDecomposer
 # BUG FIXED:  UnboundLocalError: local variable 'result' referenced before assignment
 # Caused by using more threads than spectra.
 
+
 def init_worker():
     """ Worker initializer to ignore Keyboard interrupt """
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
 
-
 def init():
     global agd_object, science_data_path, ilist, agd_data
-    [agd_object, science_data_path, ilist] = pickle.load(open('batchdecomp_temp.pickle'))
+    [agd_object, science_data_path, ilist] = pickle.load(
+        open("batchdecomp_temp.pickle")
+    )
     agd_data = pickle.load(open(science_data_path))
-    if ilist == None: ilist = np.arange(len(agd_data['x_values']))
+    if ilist == None:
+        ilist = np.arange(len(agd_data["x_values"]))
 
 
 def decompose_one(i):
-    print('   ---->  ', i) 
-    result = GaussianDecomposer.decompose(agd_object, 
-                                          agd_data['x_values'][i], 
-                                          agd_data['data_list'][i], 
-                                          agd_data['errors'][i])
+    print("   ---->  ", i)
+    result = GaussianDecomposer.decompose(
+        agd_object,
+        agd_data["x_values"][i],
+        agd_data["data_list"][i],
+        agd_data["errors"][i],
+    )
     return result
-
 
 
 def func():
 
- # Multiprocessing code
+    # Multiprocessing code
     ncpus = multiprocessing.cpu_count()
     p = multiprocessing.Pool(ncpus, init_worker)
-    if agd_object.p['verbose']: print('N CPUs: ', ncpus)
+    if agd_object.p["verbose"]:
+        print("N CPUs: ", ncpus)
     try:
         results_list = p.map(decompose_one, ilist)
 
@@ -47,5 +52,3 @@ def func():
     p.close()
     del p
     return results_list
-
-
